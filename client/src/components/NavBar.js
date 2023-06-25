e782f2338a44772eb2bd627bff0572d496767515
Yahyawi99 <yassinyahaywi26@gmail.com>
2023-06-25 17:24:20 +0100

    working on dropdowns

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -7,13 +7,14 @@ import {
   faDiagramProject,
   faTicket,
   faChevronDown,
+  faStarOfLife,
 } from "@fortawesome/free-solid-svg-icons";
 import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 // css
 import "../styles/components/navbar.css";
 
 const NavBar = () => {
-  const { clicked, setClicked } = useNavGlobal();
+  const { clicked, setClicked, dropDown, showHideDropDown } = useNavGlobal();
 
   return (
     <nav className="navbarContainer">
@@ -58,29 +59,89 @@ const NavBar = () => {
 
             <li
               className={`listItem3 ${clicked === "projects" && "clicked"}`}
-              onClick={() => setClicked("projects")}
+              onClick={(e) => {
+                setClicked("projects");
+                showHideDropDown(e, "projects");
+              }}
             >
               <i>
                 <FontAwesomeIcon icon={faDiagramProject} />
               </i>
+
               <p>Projects</p>
-              <i className="arrow">
+
+              <i
+                className={`arrow ${dropDown === "projects" && "clickedArrow"}`}
+              >
                 <FontAwesomeIcon icon={faChevronDown} />
               </i>
             </li>
 
+            <div
+              className={`options ${dropDown === "projects" && "openDropDown"}`}
+            >
+              <span>
+                <FontAwesomeIcon icon={faStarOfLife} />
+                <p>All Projects</p>
+              </span>
+
+              <span>
+                <FontAwesomeIcon icon={faStarOfLife} />
+                <p> My Projects</p>
+              </span>
+
+              <span>
+                <FontAwesomeIcon icon={faStarOfLife} />
+                <p> Archived Projects</p>
+              </span>
+            </div>
+
             <li
               className={`listItem4 ${clicked === "tickets" && "clicked"}`}
-              onClick={() => setClicked("tickets")}
+              onClick={(e) => {
+                setClicked("tickets");
+                showHideDropDown(e, "tickets");
+              }}
             >
               <i>
                 <FontAwesomeIcon icon={faTicket} />
               </i>
               <p>Tickets</p>
-              <i className="arrow">
+              <i
+                className={`arrow ${dropDown === "tickets" && "clickedArrow"}`}
+              >
                 <FontAwesomeIcon icon={faChevronDown} />
               </i>
             </li>
+
+            <div
+              className={`options ${dropDown === "tickets" && "openDropDown"}`}
+            >
+              <span>
+                <FontAwesomeIcon icon={faStarOfLife} />
+                <p>All Tickets</p>
+              </span>
+
+              <span>
+                <FontAwesomeIcon icon={faStarOfLife} />
+                <p> My Tickets</p>
+              </span>
+
+              <span>
+                <FontAwesomeIcon icon={faStarOfLife} />
+                <p> Add Ticket</p>
+              </span>
+
+              <span>
+                <FontAwesomeIcon icon={faStarOfLife} />
+                <p> Unassigned Tickets</p>
+              </span>
+
+              <span>
+                <FontAwesomeIcon icon={faStarOfLife} />
+                <p> Ticket Archive</p>
+              </span>
+            </div>
           </ul>
         </div>
       </div>
--- client/src/context/NavBar.js
+++ client/src/context/NavBar.js
@@ -3,12 +3,23 @@ import React, { useContext, useState } from "react";
 const AppContext = React.createContext();
 const NavBarProvider = ({ children }) => {
   const [clicked, setClicked] = useState("dashboard");
+  const [dropDown, setDropDown] = useState("");
+
+  const showHideDropDown = (e, value) => {
+    if (dropDown === value) {
+      setDropDown("");
+    } else {
+      setDropDown(value);
+    }
+  };
 
   return (
     <AppContext.Provider
       value={{
         clicked,
         setClicked,
+        dropDown,
+        showHideDropDown,
       }}
     >
       {children}
