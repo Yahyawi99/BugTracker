389ffd2122f6409c0d4cde3d5dcff62ea01d6e3b
Yahyawi99 <yassinyahyawi26@gmail.com>
2023-09-20 20:23:05 +0100

    working on authentication

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require("../model/User");
 const { StatusCodes } = require("http-status-codes");
 const CustomErrors = require("../errors");
+const { createTokenUser, attachCookieToResponse } = require("../utils");
 
 // Login
 const login = async (req, res) => {
@@ -43,6 +44,9 @@ const register = async (req, res) => {
   }
 
   const user = await User.create({ name, email, password });
+
+  // create token and attach cookie to response
+  const tokenUser = createTokenUser(user);
 
   res
     .status(StatusCodes.CREATED)
---
+++ server/utils/index.js
@@ -0,0 +1,4 @@
+const createTokenUser = require("./createTokenUser");
+const attachCookieToResponse = require("./attachCookieToResponse");
+
+module.exports = { createTokenUser, attachCookieToResponse };
