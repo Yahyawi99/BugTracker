d0332a7ae08683ae3842a3ff463c04dfe569fce4
Yahyawi99 <yassinyahyawi26@gmail.com>
2023-09-20 16:21:48 +0100

    create notFound middleware

--- server/app.js
+++ server/app.js
@@ -4,18 +4,24 @@ require("express-async-errors");
 const express = require("express");
 const app = express();
 
+// middlewares
+const NotFoundMiddleware = require("./middlewares/not-found");
+const errHandlerMiddleware = require("./middlewares/err-handler");
+
 // DB
 const connectDB = require("./db/connect");
 
 // Routers
 const AuthRoutes = require("./routes/authRoutes");
 
-// middlewares
 app.use(express.json());
 
 // ========================================
 // ========================================
 app.use("/api/v1", AuthRoutes);
+
+app.use(NotFoundMiddleware);
+app.use(errHandlerMiddleware);
 
 // Start
 const port = process.env.PORT || 5000;
---
+++ server/middlewares/not-found.js
@@ -0,0 +1,6 @@
+const { StatusCodes } = require("http-status-codes");
+
+const notFound = (req, res) =>
+  res.status(StatusCodes.NOT_FOUND).json({ msg: "Route doesn't existe" });
+
+module.exports = notFound;
