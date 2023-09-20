ba25eeba25fd8e784ec06ed348e2ac54d64a314d
Yahyawi99 <yassinyahyawi26@gmail.com>
2023-09-20 16:25:38 +0100

    create notFound middleware

--- server/errors/Bad-request.js
+++ server/errors/Bad-request.js
@@ -1,6 +1,7 @@
 const { StatusCodes } = require("http-status-codes");
+const CustomError = require("./CustomError");
 
-class BadRequestError extends Error {
+class BadRequestError extends CustomError {
   constructor(message) {
     super(message);
     this.statusCode = StatusCodes.BAD_REQUEST;
--- server/middlewares/err-handler.js
+++ server/middlewares/err-handler.js
@@ -1,6 +1,6 @@
 const { StatusCodes } = require("http-status-codes");
 
-const errHandler = (err, req, res) => {
+const errHandler = (err, req, res, next) => {
   let customError = {
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
     message: err.message || "Something went wrong please try again!",
