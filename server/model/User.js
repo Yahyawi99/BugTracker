fa4888c14ef208ee486de570540fc50372799c21
Yahyawi99 <yassinyahyawi26@gmail.com>
2023-09-20 16:58:35 +0100

    add hash password methode

--- server/model/User.js
+++ server/model/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcrypt");
 
 const UserSchema = new mongoose.Schema(
   {
@@ -35,5 +36,12 @@ const UserSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+UserSchema.pre("save", async function () {
+  if (this.isModified(this.password)) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+  }
+});
 
 module.exports = mongoose.model("User", UserSchema);
