1c4eb13d585728812159d360e90ea3ceab958668
Yahyawi99 <yassinyahyawi26@gmail.com>
2023-09-21 22:08:56 +0100

    working on demo

---
+++ client/src/components/Login/Demo.js
@@ -0,0 +1,17 @@
+import React from "react";
+// css
+import "../../styles/pages/Login/demo.css";
+
+const DemoAccounts = () => {
+  return (
+    <section className="demoAccountsContainer">
+      <button>admin</button>
+      <button>developer</button>
+      <button>project manager</button>
+      <button>user</button>
+      <button>submitter</button>
+    </section>
+  );
+};
+
+export default DemoAccounts;
