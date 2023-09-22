45295316ae61f0b55862fa4a276b3dd0ed3d611b
Yahyawi99 <yassinyahyawi26@gmail.com>
2023-09-22 17:30:11 +0100

    create populate.js

---
+++ server/populate.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+
+const connectDB = require("./db/connect");
+
+const Project = require("./model/Project");
+const mockData = require("./data/mock-projects.json");
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    await Project.deleteMany();
+    await Project.create(mockData);
+
+    console.log("success");
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+start();
