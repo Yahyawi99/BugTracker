e157521054f3530f26f8a164466ef7f9df41d5de
Yahyawi99 <yassinyahyawi26@gmail.com>
2023-09-22 22:06:42 +0100

    add assign property

--- server/model/User.js
+++ server/model/User.js
@@ -33,12 +33,20 @@ const UserSchema = new mongoose.Schema(
       enum: ["admin", "developer", "PM", "submitter"],
       default: "submitter",
     },
+
+    onProject: {
+      type: mongoose.Types.ObjectId,
+      ref: "project",
+    },
+
+    assignedTo: { type: mongoose.Types.ObjectId, ref: "ticket" },
   },
   { timestamps: true }
 );
 
 // hash the password after every save
 UserSchema.pre("save", async function () {
+  // hash the password if modified
   if (this.isModified("password")) {
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
@@ -48,6 +56,11 @@ UserSchema.pre("save", async function () {
 // compare passwords
 UserSchema.methods.ComparePasswords = async function (inputPassword) {
   return bcrypt.compare(inputPassword, this.password);
+};
+
+UserSchema.methods.assignTo = async function (ticketId) {
+  UserSchema.add({});
+  this.assignedTo = ticketId;
 };
 
 module.exports = mongoose.model("User", UserSchema);
