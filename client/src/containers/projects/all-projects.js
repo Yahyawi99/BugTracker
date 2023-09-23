e712fc4541d5313b130cb0a347bd65a47e51cfeb
Yahyawi99 <yassinyahyawi26@gmail.com>
2023-09-23 16:09:55 +0100

    stylying

--- client/src/containers/projects/all-projects.js
+++ client/src/containers/projects/all-projects.js
@@ -18,41 +18,79 @@ const AllProjects = () => {
 
       <div>
         <div>
-          <div className="sectionHeader"></div>
-        </div>
-
-        <div className="labels">
           <div>
-            <p>Project</p>
+            <div className="sectionHeader"></div>
+          </div>
+
+          <div className="labels">
+            {[
+              "Project",
+              "End Date",
+              "Progress",
+              "Project Manager",
+              "Team",
+              "Status",
+              "",
+            ].map((label) => {
+              return (
+                <div>
+                  <p>{label}</p>
+
+                  {label && (
+                    <i>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M12 0l-8 10h16l-8-10zm3.839 16l-3.839 4.798-3.839-4.798h7.678zm4.161-2h-16l8 10 8-10z" />
+                      </svg>
+
+                      {/* <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M12 3.202l3.839 4.798h-7.678l3.839-4.798zm0-3.202l-8 10h16l-8-10zm8 14h-16l8 10 8-10z" />
+                    </svg> */}
+                    </i>
+                  )}
+                </div>
+              );
+            })}
+            {/* <div>
+              <p>Project</p>
+            </div>
+
+            <div>
+              <p>End Date</p>
+            </div>
+
+            <div>
+              <p>Progress</p>
+            </div>
+
+            <div>
+              <p>Project Manager</p>
+            </div>
+
+            <div>
+              <p>Team</p>
+            </div>
+
+            <div>
+              <p>Status</p>
+            </div>
+
+            <div></div> */}
           </div>
 
           <div>
-            <p>End Date</p>
-          </div>
+            <div className="projectName"></div>
 
-          <div>
-            <p>Progress</p>
-          </div>
-
-          <div>
-            <p>Project Manager</p>
-          </div>
-
-          <div>
-            <p>Team</p>
-          </div>
-
-          <div>
-            <p>Status</p>
-          </div>
-
-          <div></div>
-        </div>
-
-        <div>
-          <div className="projectName"></div>
-
-          {/* <div className="date"></div>
+            {/* <div className="date"></div>
 
           <div className="progress"></div>
 
@@ -63,6 +101,7 @@ const AllProjects = () => {
           <div className="status"></div>
 
           <div className="btns"></div> */}
+          </div>
         </div>
       </div>
     </section>
