029a6174a2a26bb13fa8a20a29d9c3fe071e652d
Yahyawi99 <yassinyahyawi26@gmail.com>
2023-10-02 16:58:34 +0100

    implement note-editor

--- client/src/containers/projects/EditProject.js
+++ client/src/containers/projects/EditProject.js
@@ -1,43 +1,85 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactQuill from "react-quill";
-import "react-quill/dist/quill.snow.css";
+import useProjects from "../../hooks/useProjects";
 // compenents
 import HomeBtn from "../../components/shared/HomeBtn";
 // css
 import "../../styles/containers/projects/edit-project.css";
+import "react-quill/dist/quill.snow.css";
+import { useParams } from "react-router-dom";
+
+const modules = {
+  toolbar: [
+    [{ font: [] }],
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    ["bold", "italic", "underline", "strike"],
+    [{ color: [] }, { background: [] }],
+    ["blockquote", "code-block"],
+    [{ list: "ordered" }, { list: "bullet" }],
+    [{ indent: "-1" }, { indent: "+1" }, { align: [] }],
+  ],
+};
 
 const EditProject = () => {
+  const { getSingleProject, singleProject } = useProjects();
+  const { projectId } = useParams();
+
+  const [project, setProject] = useState({});
+
+  useEffect(() => {
+    getSingleProject(projectId);
+  }, []);
+
+  useEffect(() => {
+    setProject(singleProject.project);
+  }, [singleProject]);
+
   return (
-    <section className="editProject">
-      <HomeBtn name="Edit" />
+    !!project && (
+      <section className="editProject">
+        <HomeBtn name="Edit" />
 
-      <div className="editForm">
-        <div>
-          <label htmlFor="name">Project Name</label>
-          <input type="text" id="name" />
+        <div className="editForm">
+          <div>
+            <label htmlFor="name">Project Name</label>
+            <input
+              type="text"
+              id="name"
+              placeholder="project name"
+              value={project.name}
+              onChange={(e) =>
+                setProject({ ...project, name: e.currentTarget.value })
+              }
+            />
+          </div>
+
+          <div className="note-editor">
+            <label htmlFor="description">Project Description</label>
+            <ReactQuill
+              modules={modules}
+              theme="snow"
+              style={{ height: "200px" }}
+              placeholder="Your project description..."
+            />
+          </div>
+
+          <div>
+            <div className="startDate"></div>
+            <div className="endDate"></div>
+          </div>
+
+          <div>
+            <label htmlFor="priority">Choose a priority</label>
+          </div>
+
+          <div>
+            <label htmlFor="manager">Project Manager</label>
+          </div>
+
+          <button type="button">Save Changes</button>
         </div>
-
-        <div>
-          <label htmlFor="description">Project Description</label>
-          <ReactQuill theme="snow" />
-        </div>
-
-        <div>
-          <div className="startDate"></div>
-          <div className="endDate"></div>
-        </div>
-
-        <div>
-          <label htmlFor="priority">Choose a priority</label>
-        </div>
-
-        <div>
-          <label htmlFor="manager">Project Manager</label>
-        </div>
-
-        <button type="button">Save Changes</button>
-      </div>
-    </section>
+      </section>
+    )
   );
 };
 
