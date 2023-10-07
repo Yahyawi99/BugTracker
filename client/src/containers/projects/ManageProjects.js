906381f3e61ce247381e1b5e26b1497e55613565
Yahyawi99 <yassinyahyawi26@gmail.com>
2023-10-07 11:56:54 +0100

    working on manage project component

--- client/src/containers/projects/ManageProjects.js
+++ client/src/containers/projects/ManageProjects.js
@@ -1,4 +1,8 @@
 import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+// icons
+import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // hooks
 import useProjects from "../../hooks/useProjects";
 // components
@@ -7,31 +11,90 @@ import HomeBtn from "../../components/shared/HomeBtn";
 import "../../styles/containers/projects/manage-projects.css";
 
 const ManageProjects = () => {
-  const { getAllProjects, allProjects } = useProjects();
+  const { getAllProjects, allProjects, archiveProject } = useProjects();
 
   useEffect(() => {
     getAllProjects(1, "", "", "", "all");
   }, []);
 
   return (
-    <section>
+    <section className="ManageProjectsSection">
       <HomeBtn name="Manage Projects" />
 
       <div className="projectsContainer">
         {allProjects.projects &&
           allProjects.projects.map((project) => {
-            const { _id, name } = project;
+            const { _id, name, priority, isArchive, description, team } =
+              project;
 
             return (
               <div key={_id} className="project">
-                <div></div>
-                <div></div>
-                <div></div>
+                <div>
+                  <div>
+                    <p className="name">{name}</p>
+                    <p className="priority">{priority}</p>
+                  </div>
+
+                  <i className="arrow-down">
+                    <FontAwesomeIcon icon={faAngleDown} />
+                  </i>
+
+                  <DropDownToggle
+                    projectId={_id}
+                    isArchive={isArchive}
+                    archiveProject={archiveProject}
+                  />
+                </div>
+
+                <div>
+                  <p dangerouslySetInnerHTML={{ __html: description }} />
+                </div>
+
+                <div>
+                  <div>
+                    <p>Team :</p>
+                    <div className="team">
+                      {team.map((user) => {
+                        const { avatar } = user;
+
+                        return <img src={avatar} alt="user" />;
+                      })}
+                    </div>
+                  </div>
+
+                  <button type="button">Manage Team</button>
+                </div>
+
+                <div>
+                  <p>Tickets</p>
+                  <p>0</p>
+                </div>
               </div>
             );
           })}
       </div>
     </section>
+  );
+};
+
+// **************************
+const DropDownToggle = (props) => {
+  const { projectId, isArchive, archiveProject } = props;
+
+  return (
+    <div className="dropDown">
+      <Link to={`/projects/project-details/${projectId}`}>
+        <button>View</button>
+      </Link>
+
+      <Link to={`/projects/edit-project/${projectId}`}>
+        <button>Edit</button>
+      </Link>
+
+      <button onClick={() => archiveProject(projectId, !isArchive)}>
+        Archive
+      </button>
+    </div>
   );
 };
 
