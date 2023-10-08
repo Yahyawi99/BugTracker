83df1c3831caf605feebe1aaeba2922f6094d4db
Yahyawi99 <yassinyahyawi26@gmail.com>
2023-10-08 13:17:18 +0100

    working on my tickets details page

---
+++ client/src/containers/tickets/ticketDetails.js
@@ -0,0 +1,9 @@
+import React from "react";
+// css
+import "../../styles/containers/tickets/ticket-details.css";
+
+const TicketDetails = () => {
+  return <div>TicketDetails</div>;
+};
+
+export default TicketDetails;
