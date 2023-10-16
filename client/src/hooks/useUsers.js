a5a1a5cb08572e1cb6e4909c2761304cd990ce52
Yahyawi99 <yassinyahyawi26@gmail.com>
2023-10-16 12:54:46 +0100

    single user hook

--- client/src/hooks/useUsers.js
+++ client/src/hooks/useUsers.js
@@ -5,6 +5,7 @@ import { useMainContext } from "../context/global";
 const useUsers = () => {
   const { alertMe, loading } = useMainContext();
   const [allUsers, setAllUsers] = useState([]);
+  const [singleUser, setSingleUser] = useState({});
 
   //   get all users
   const getAllUsers = async () => {
@@ -21,12 +22,36 @@ const useUsers = () => {
     } catch (error) {
       loading(false);
 
-      const msg = error.response.data.msg;
-      await alertMe(msg + "!", "var(--danger)");
+      await alertMe(
+        "Something went wrong please try again later!" + "!",
+        "var(--danger)"
+      );
     }
   };
 
-  return { getAllUsers, allUsers };
+  //   get single user
+  const getSingleUser = async (memberId) => {
+    try {
+      loading(true);
+
+      const response = await axios.get(`/api/v1/user/${memberId}`);
+
+      loading(false);
+
+      setSingleUser(response.data);
+
+      await alertMe("Done.", "var(--success)");
+    } catch (error) {
+      loading(false);
+
+      await alertMe(
+        "Something went wrong please try again later!" + "!",
+        "var(--danger)"
+      );
+    }
+  };
+
+  return { getAllUsers, allUsers, getSingleUser, singleUser };
 };
 
 export default useUsers;
