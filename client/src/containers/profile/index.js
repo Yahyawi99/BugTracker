a8107d943fd7706c8da224a2ec72eb416372697e
Yahyawi99 <yassinyahyawi26@gmail.com>
2023-10-16 13:25:30 +0100

    profile frontend

--- client/src/containers/profile/index.js
+++ client/src/containers/profile/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
 // hooks
 import useUsers from "../../hooks/useUsers";
@@ -15,33 +16,33 @@ const MemberProfile = () => {
   console.log(singleUser);
 
   return (
-    singleUser && (
-      <section className="memberProfile">
-        <div className="memberInfo">
-          <img src={singleUser.avatar} alt="member" />
+    <section className="memberProfile">
+      <div className="memberInfo">
+        <img src={singleUser.avatar} alt="member" />
 
-          <h1 className="name">{singleUser.name} </h1>
+        <h1 className="name">{singleUser.name} </h1>
 
-          <p className="role">{singleUser.role} </p>
+        <p className="role">{singleUser.role} </p>
 
-          <div className="email">
-            <p>Email address : </p>
-            <a href={`mailto:${singleUser.email}`}>{singleUser.email}</a>
-          </div>
-
-          <div className="phone">
-            <p>Mobile : </p>
-            {singleUser.phoneNumber ? (
-              <a href={`tel:${singleUser.phoneNumber}`}>
-                {singleUser.phoneNumber}
-              </a>
-            ) : (
-              <p>No phone number</p>
-            )}
-          </div>
+        <div className="email">
+          <p>Email address : </p>
+          <Link to={`mailto:${singleUser.email}`}>
+            <p>{singleUser.email}</p>
+          </Link>
         </div>
-      </section>
-    )
+
+        <div className="phone">
+          <p>Mobile : </p>
+          {singleUser.phoneNumber ? (
+            <a href={`tel:${singleUser.phoneNumber}`}>
+              {singleUser.phoneNumber}
+            </a>
+          ) : (
+            <p>No phone number</p>
+          )}
+        </div>
+      </div>
+    </section>
   );
 };
 
