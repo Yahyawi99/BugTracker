7d5a0e315676b8da8dda8c71f72b8f068470e7db
Yassin Yahyawi <yassinyahyawi26@gmail.com>
2023-10-21 19:55:05 +0100

    styling manage profile comp

--- client/src/containers/profile/manage-profile.js
+++ client/src/containers/profile/manage-profile.js
@@ -10,43 +10,45 @@ const ManageProfile = () => {
       <HomeBtn name="Profile" />
 
       <div>
-        <h1>Manage your account</h1>
-        <h2>Change your account settings</h2>
-      </div>
-
-      <div>
-        <ul className="navigation">
-          <li>Profile</li>
-          <li>Email</li>
-          <li>Password</li>
-        </ul>
+        <div className="titles">
+          <h1>Manage your account</h1>
+          <h2>Change your account settings</h2>
+        </div>
 
         <div>
-          <h3>Profile</h3>
+          <ul className="navigation">
+            <li className="clicked">Profile</li>
+            <li>Email</li>
+            <li>Password</li>
+          </ul>
 
-          <form>
-            <div className="nameController">
-              <label htmlFor="username">Username</label>
-              <input type="text" id="username" />
-            </div>
+          <div>
+            <h3>Profile</h3>
 
-            <div className="phoneController">
-              <label htmlFor="phone">Phone number</label>
-              <input type="tel" id="phone" />
-            </div>
-
-            <div>
-              <div className="imageController">
-                <label htmlFor="image">Image</label>
-                <input type="image" id="image" />
+            <form>
+              <div className="nameController">
+                <label htmlFor="username">Username</label>
+                <input type="text" id="username" />
               </div>
 
-              <div className="currentImageController">
-                <label htmlFor="currentImage">Current Image</label>
-                <img src="j" alt="member" id="currentImage" />
+              <div className="phoneController">
+                <label htmlFor="phone">Phone number</label>
+                <input type="tel" id="phone" />
               </div>
-            </div>
-          </form>
+
+              <div>
+                <div className="imageController">
+                  <label htmlFor="image">Image</label>
+                  <input type="file" id="image" />
+                </div>
+
+                <div className="currentImageController">
+                  <label htmlFor="currentImage">Current Image</label>
+                  <img src="j" alt="member" id="currentImage" />
+                </div>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </section>
