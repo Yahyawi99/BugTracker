708264c3aba07d6e630b001adba383637f27b2c7
Yassin Yahyawi <yassinyahyawi26@gmail.com>
2023-10-22 00:39:14 +0100

    styling manage profile comp

--- client/src/containers/profile/manage-profile.js
+++ client/src/containers/profile/manage-profile.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 // Components
 import HomeBtn from "../../components/shared/HomeBtn";
 // css
 import "../../styles/containers/profile/manage-profile.css";
 
 const ManageProfile = () => {
+  const [navigateTo, setNavigateTo] = useState("Profile");
+
   return (
     <section className="manageProfile">
       <HomeBtn name="Profile" />
@@ -17,105 +19,118 @@ const ManageProfile = () => {
 
         <div>
           <ul className="navigation">
-            <li className="clicked">Profile</li>
-            <li>Email</li>
-            <li>Password</li>
+            {["Profile", "Email", "Password"].map((value) => {
+              return (
+                <li
+                  className={`${navigateTo === value && "clicked"}`}
+                  onClick={() => setNavigateTo(value)}
+                >
+                  {value}
+                </li>
+              );
+            })}
           </ul>
 
           <div>
-            <h3>Password</h3>
+            <h3>{navigateTo}</h3>
 
-            {/* <form className="form profile-form">
-              <div className="nameController">
-                <label className="label" htmlFor="username">
-                  Username
-                </label>
-                <input type="text" id="username" />
-              </div>
-
-              <div className="phoneController">
-                <label className="label" htmlFor="phone">
-                  Phone number
-                </label>
-                <input type="tel" id="phone" />
-              </div>
-
-              <div className="imageContainer">
-                <div className="imageController">
-                  <p className="label">Image</p>
-
-                  <input type="file" id="image" />
-
-                  <label htmlFor="image" class="custom-button">
-                    Choose File
+            {navigateTo === "Profile" && (
+              <form className="form profile-form">
+                <div className="nameController">
+                  <label className="label" htmlFor="username">
+                    Username
                   </label>
-
-                  <span className="file-name">No file chosen</span>
+                  <input type="text" id="username" />
                 </div>
 
-                <div className="currentImageController">
-                  <label className="label" htmlFor="currentImage">
-                    Current Image
+                <div className="phoneController">
+                  <label className="label" htmlFor="phone">
+                    Phone number
                   </label>
+                  <input type="tel" id="phone" />
+                </div>
 
-                  <img
-                    src="/assets/images/default-avatar-1.jpg"
-                    alt="member"
-                    id="currentImage"
+                <div className="imageContainer">
+                  <div className="imageController">
+                    <p className="label">Image</p>
+
+                    <input type="file" id="image" />
+
+                    <label htmlFor="image" class="custom-button">
+                      Choose File
+                    </label>
+
+                    <span className="file-name">No file chosen</span>
+                  </div>
+
+                  <div className="currentImageController">
+                    <label className="label" htmlFor="currentImage">
+                      Current Image
+                    </label>
+
+                    <img
+                      src="/assets/images/default-avatar-1.jpg"
+                      alt="member"
+                      id="currentImage"
+                    />
+                  </div>
+                </div>
+              </form>
+            )}
+
+            {navigateTo === "Email" && (
+              <form className="form email-form">
+                <div>
+                  <label htmlFor="oldEmail" className="label">
+                    Email
+                  </label>
+                  <input
+                    id="oldEmail readonlyInput"
+                    type="text"
+                    value="yassinyahyawi26@gmail.com"
+                    readOnly
                   />
                 </div>
-              </div>
-            </form> */}
 
-            {/* <form className="form email-form">
-              <div>
-                <label htmlFor="oldEmail" className="label">
-                  Email
-                </label>
-                <input
-                  id="oldEmail readonlyInput"
-                  type="text"
-                  value="yassinyahyawi26@gmail.com"
-                  readOnly
-                />
-              </div>
+                <div>
+                  <label htmlFor="newEmail" className="label">
+                    New Email
+                  </label>
+                  <input id="newEmail" type="email" />
+                </div>
 
-              <div>
-                <label htmlFor="newEmail" className="label">
-                  New Email
-                </label>
-                <input id="newEmail" type="email" />
-              </div>
+                <button className="Btn">Change email</button>
+              </form>
+            )}
 
-              <button className="Btn">Change email</button>
-            </form> */}
+            {navigateTo === "Password" && (
+              <form className="form password-form">
+                <div>
+                  <label htmlFor="currentPass" className="label">
+                    Current password
+                  </label>
+                  <input type="password" id="currentPass" />
+                </div>
 
-            <form className="form password-form">
-              <div>
-                <label htmlFor="currentPass" className="label">
-                  Current password
-                </label>
-                <input type="password" id="currentPass" />
-              </div>
+                <div>
+                  <label htmlFor="newPass" className="label">
+                    New password
+                  </label>
+                  <input type="password" id="newPass" />
+                </div>
 
-              <div>
-                <label htmlFor="newPass" className="label">
-                  New password
-                </label>
-                <input type="password" id="newPass" />
-              </div>
+                <div>
+                  <label htmlFor="confirmNewPass" className="label">
+                    Confirm new password
+                  </label>
+                  <input type="password" id="confirmNewPass" />
+                </div>
 
-              <div>
-                <label htmlFor="confirmNewPass" className="label">
-                  Confirm new password
-                </label>
-                <input type="password" id="confirmNewPass" />
-              </div>
-
-              <button type="submit" className="Btn">
-                Update password
-              </button>
-            </form>
+                <button type="submit" className="Btn">
+                  Update password
+                </button>
+              </form>
+            )}
           </div>
         </div>
       </div>
