35f1c1389c7fbb98155d7d123632f226ef276f9a
Yassin Yahyawi <yassinyahyawi26@gmail.com>
2023-10-25 19:01:18 +0100

    not found page

--- client/src/containers/profile/index.js
+++ client/src/containers/profile/index.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 // components
 import MemberProfile from "./member-profile";
 import ManageProfile from "./manage-profile";
+import NotFound from "../../components/not-found";
 
 const Profile = () => {
   return (
@@ -10,6 +11,8 @@ const Profile = () => {
       <Routes>
         <Route path="/member-profile/:memberId" Component={MemberProfile} />
         <Route path="/manage-profile" Component={ManageProfile} />
+
+        <Route path="*" Component={NotFound} />
       </Routes>
     </>
   );
