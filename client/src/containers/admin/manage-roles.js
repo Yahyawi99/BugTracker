a38bdfb35d5f6b2da5f5e8168faf6c88912081fc
Yassin Yahyawi <yassinyahyawi26@gmail.com>
2023-10-26 18:39:13 +0100

    manage roles

--- client/src/containers/admin/manage-roles.js
+++ client/src/containers/admin/manage-roles.js
@@ -1,8 +1,23 @@
 import React from "react";
+// hooks
+import useUsers from "../../hooks/useUsers";
+// components
+import ShowAllDocuments from "../../components/shared/ShowAllDocuments";
 // css
+import "../../styles/containers/admin/manage-roles.css";
 
 const ManageRoles = () => {
-  return <div>manage-roles</div>;
+  const { getAllUsers, allUsers } = useUsers();
+
+  return (
+    <ShowAllDocuments
+      sectionName="Manage Roles"
+      controller={getAllUsers}
+      labels={["Name", "Current Role", "Manage Role", "Action"]}
+      sortLabels={["Name", "Current Role"]}
+      data={allUsers}
+    />
+  );
 };
 
 export default ManageRoles;
