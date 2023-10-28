a67badc1b2d2755b6d2d86b14286b64da424a218
Yassin Yahyawi <yassinyahyawi26@gmail.com>
2023-10-28 20:02:56 +0100

    working on manage team

--- client/src/containers/admin/manage-team.js
+++ client/src/containers/admin/manage-team.js
@@ -1,9 +1,47 @@
 import React from "react";
+// components
+import HomeBtn from "../../components/shared/HomeBtn";
 // css
 import "../../styles/containers/admin/manage-team.css";
 
 const ManageTeam = () => {
-  return <div>manage-team</div>;
+  return (
+    <section className="manageTeam">
+      <HomeBtn name="Manage Team" />
+
+      <div className="first-column">
+        <h1>Project Team</h1>
+        <p>8 team members</p>
+
+        <div className="pm">
+          <img
+            src="/assets/images/default-avatar-1.jpg"
+            alt="project-manager"
+          />
+
+          <div className="info">
+            <p>Jane Appuser</p>
+            <p>yassinyahyawi@26gmail.com</p>
+            <p>Project Manager</p>
+            <button type="button">Manage PM</button>
+          </div>
+        </div>
+
+        <div className="team">
+          <div>
+            <img src="/assets/images/default-avatar-1.jpg" alt="developer" />
+
+            <div>
+              <p>James Appuser</p>
+              <p>Developer</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="second-column"></div>
+    </section>
+  );
 };
 
 export default ManageTeam;
