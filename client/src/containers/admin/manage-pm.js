fb185372d613451bc3853e85905b38cc9c93b9ef
Yassin Yahyawi <yassinyahyawi26@gmail.com>
2023-10-30 13:59:43 +0100

    manage PM

--- client/src/containers/admin/manage-pm.js
+++ client/src/containers/admin/manage-pm.js
@@ -1,7 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+// icon
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+// hooks
+import useUsers from "../../hooks/useUsers";
+// components
+import HomeBtn from "../../components/shared/HomeBtn";
+// css
+import "../../styles/containers/admin/manage-pm.css";
 
 const ManagePM = () => {
-  return <div>manage-pm</div>;
+  const { getAllUsers, allUsers } = useUsers();
+  const [managers, setManagers] = useState([]);
+
+  useEffect(() => {
+    getAllUsers();
+  }, []);
+
+  useEffect(() => {
+    if (allUsers.users) {
+      setManagers(() => allUsers.users.filter((user) => (user.role = "PM")));
+    }
+  }, [allUsers]);
+
+  return (
+    <section className="managePM">
+      <HomeBtn name="Assign Project Manager" />
+
+      <div>
+        <h2>Land Research Web Application</h2>
+        <h4>Select Project Manager</h4>
+
+        <Dropdown managers={managers} />
+      </div>
+    </section>
+  );
+};
+
+// **********************
+const Dropdown = ({ managers }) => {
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  return (
+    <div className="dropDown">
+      <div
+        className="initialValue"
+        onClick={() => setShowDropdown(!showDropdown)}
+      >
+        <p>None selected</p>
+        <FontAwesomeIcon icon={faChevronDown} />
+      </div>
+
+      <div className={`${showDropdown && "showOptions"} options`}>
+        {managers.map((manager) => {
+          const { _id, name } = manager;
+
+          return (
+            <p key={_id} onClick={() => setShowDropdown(false)}>
+              {name}
+            </p>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default ManagePM;
