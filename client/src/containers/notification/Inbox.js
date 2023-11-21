e9d12732a438727c6f3eb8e8960af1909c5227ba
Yahyawi99 <yassinyahyawi26@gmail.com>
2023-11-21 22:54:21 +0100

    working on notification

--- client/src/containers/notification/Inbox.js
+++ client/src/containers/notification/Inbox.js
@@ -1,7 +1,45 @@
 import React from "react";
+// icons
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+// components
+import HomeBtn from "../../components/shared/HomeBtn";
+// css
+import "../../styles/containers/notification/inbox.css";
 
 const Inbox = () => {
-  return <div>Inbox</div>;
+  return (
+    <section className="inboxContainer">
+      <HomeBtn name="Notifications" />
+
+      <div>
+        <div className="menu">
+          <button>Compose</button>
+
+          <div>
+            <FontAwesomeIcon icon={faEnvelope} />
+            <p>New</p>
+            <p>0</p>
+          </div>
+        </div>
+
+        <div className="mail">
+          <div>
+            <h3>Inbox</h3>
+            <button>Menu</button>
+          </div>
+
+          <form className="search">
+            <input type="text" placeholder="Search Mail" />
+            <button type="button">
+              <FontAwesomeIcon icon={faMagnifyingGlass} />
+            </button>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Inbox;
